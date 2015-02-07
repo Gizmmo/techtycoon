d267e81b54bdb9fc73458136195f3b433c7dadd6
Travis Scott <travisscott301@gmail.com>
2015-02-06 23:53:06 -0600

    Added Movement for employees

---
+++ Assets/Scripts/ChairStatus.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChairStatus : MonoBehaviour {
+	bool _isChairOpen = true;
+	GameObject _occupant;
+
+	public bool isChairOpen {
+		get {
+			return _isChairOpen;
+		}
+
+		set {
+			_isChairOpen = value;
+		}
+	}
+
+	public GameObject occupant {
+		get {
+			return _occupant;
+		}
+
+		set {
+			_occupant = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
