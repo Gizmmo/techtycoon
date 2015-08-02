4fdd9f674c732d90e99fb2d7868c5abcd65cc050
Travis Scott <travisscott301@gmail.com>
2015-08-02 14:55:56 -0500

    Added Unity Seed to project

---
+++ Assets/Scripts/Generic Scripts/Framework/Extensions/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+	protected static T InnerInstance;
+
+	/**
+	   Returns the instance of this singleton.
+	*/
+	public static T Instance {
+		get {
+			if(InnerInstance != null) {
+				return InnerInstance;
+			}
+
+			InnerInstance = (T)FindObjectOfType(typeof(T));
+
+			if(InnerInstance == null) {
+				Debug.LogError("An instance of " + typeof(T) + " is needed in the scene, but there is none.");
+			}
+
+			return InnerInstance;
+		}
+	}
+}
---
+++ Assets/Scripts/Generic Scripts/Framework/FSM/IFSMState.cs
@@ -0,0 +1,13 @@
+﻿public interface IFsmState {
+
+    /// <summary>
+    /// Called when the state is set to the current state
+    /// </summary>
+    void OnEntry();
+
+    /// <summary>
+    /// Called when the state is removed as the current state
+    /// </summary>
+    void OnExit();
+
+}
